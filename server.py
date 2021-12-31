ebe03e6c89bcbf49976c3a8d77347c737c4ecf2e
hung-le-10 <84281898+hung-le-10@users.noreply.github.com>
2021-12-31 21:33:33 +0700

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import select
 HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
-PORT = 65000
+PORT = 8080
 
 # Create a socket
 # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
